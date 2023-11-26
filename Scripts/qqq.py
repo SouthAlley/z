2360c02be4630ec51490da283452a79119128725
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-11-26 15:58:46 +0800

    Update qqq.py

--- Scripts/qqq.py
+++ Scripts/qqq.py
@@ -1,12 +1,15 @@
 # 从文件中读取域名规则列表
-with open('Surge/Rule/MyBlockAds.list', 'r') as file:
+with open('Scripts/MyBlockAds.list', 'r') as file:
     rules = file.read().splitlines()
 
 # 定义规则顺序
 rule_order = ['DOMAIN', 'DOMAIN-SUFFIX', 'DOMAIN-KEYWORD', 'IP-CIDR', 'USER-AGENT']
 
 # 对规则进行排序
-sorted_rules = sorted(rules, key=lambda x: rule_order.index(x.split(',')[0]))
+sorted_rules = sorted(
+    filter(lambda x: x.split(',')[0] in rule_order, rules),
+    key=lambda x: rule_order.index(x.split(',')[0])
+)
 
 # 输出排序后的规则
 for rule in sorted_rules:
