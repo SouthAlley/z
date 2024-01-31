1c727598b5a5709abd6a43d06be0fe5f12f30d11
GitHub Action <action@github.com>
2024-01-31 01:00:29 +0000

    Copy lodepuly and RuCu6 Scripts

--- Scripts/lodepuly/Reddit/Reddit_remove_ads.js
+++ Scripts/lodepuly/Reddit/Reddit_remove_ads.js
@@ -1,16 +1,12 @@
-/**
- * @author fmz200
- * @function Reddit过滤推广，关NSFW提示
- * @date 2024-01-2 17:01:00
- * @quote xream
- */
-
+/*
+脚本引用 @xream
+*/
 let body;
 try {
   body = JSON.parse($response.body.replace(/"isNsfw":true/g, '"isNsfw":false'))
   if (body.data?.children?.commentsPageAds) {
     body.data.children.commentsPageAds = []
-  }
+  } 
   for (const [k, v] of Object.entries(body.data)) {
     if (v?.elements?.edges) {
       body.data[k].elements.edges = v.elements.edges.filter(
@@ -22,8 +18,9 @@ try {
     }
   }
 
+  
 } catch (e) {
   console.log(e);
 } finally {
-  $done(body ? {body: JSON.stringify(body)} : {});
+  $done(body ? { body: JSON.stringify(body) } : {});
 }
